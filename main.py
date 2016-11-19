2c6a33630f8e4a1a6d357e5ad40a2a0776c1b552
kvaghel1 <kvaghel1@binghamton.edu>
2016-11-19 09:39:32 -0500

    update main.py -- timezone|error message
    time zone for league fixture.
    Error message commands.
    team fetching in progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from prettytable import PrettyTable
+from datetime import date
 
 import http.client
 import json
@@ -35,6 +36,27 @@ def get_league_id(league_name):
 		"440: CL(Champions League 2016/17) \n" 
 		print(list_of_competitions)
 
+def get_team_id(team_name):
+	connection = http.client.HTTPConnection('api.football-data.org')
+	headers = { 'X-Auth-Token': '8b1b8ec671d745a0b7265401137fcf0e', 'X-Response-Control': 'minified' }
+	
+	#total teams: 1 to 851 	
+	for i in range(1,852):
+		print (i)
+		connection.request('GET', '/v1/teams/'+str(i)+'/', None, headers )
+		response = json.loads(connection.getresponse().read().decode())
+		print (response)
+		if(response['shortName'].upper() == team_name.upper()):
+			return i
+	
+#--team
+def show_team(team_name):
+	print (get_team_id(team_name))
+
+def to_day_time(a):
+	#2016-11-19T12:30:00Z
+	day_list = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+	return (day_list[datetime.date(int(a[0:4]),int(a[5:7]),int(a[8:10])).weekday()] + " " + str(int((a[11:13])) - 5 % 24)+str(a[13:16]) )
 
 def show_next_game(league_name):
 	league_id = get_league_id(league_name)
@@ -45,13 +67,11 @@ def show_next_game(league_name):
 	connection.request('GET', '/v1/competitions/'+str(league_id)+'/fixtures', None, headers )
 	
 	response = json.loads(connection.getresponse().read().decode())
-	
+	t = PrettyTable(['Home Team' ,'Away Team','Day - Time'])
 	for i in response['fixtures']:
 		if(i['status'] == "TIMED"):
-			print (i['homeTeamName'] + " v/s " + i['awayTeamName'] + " "+i['date'] + "\n" )
-
-
-
+			t.add_row([i['homeTeamName'],  i['awayTeamName'] ,to_day_time(i['date'])] )
+	print (t)
 
 def show_league_table(league_name):
 	league_id = get_league_id(league_name)
@@ -67,27 +87,48 @@ def show_league_table(league_name):
 			t.add_row([ i['team'], i['playedGames'], i['goals'], i['goalsAgainst'], i['goalDifference'], i['points'] ])
 		print (t)
 
+def show_help():
+	help_message = "Please find the following commands for reference: \n"
+	t = PrettyTable(["Command" , "Description"])
+	t.add_row([ "--nextweek LEAGUE_CODE " , " Show the next fixtures of the league" ])
+	t.add_row([ "--league LEAGUE_CODE " , " Show the current league standings of the league" ])
+	print (t)
+
+
 def main():
 	if (len(sys.argv) == 1):
 		print ("No argument Entered !")
+		print ("use --help for the list of command \n")
 
-	elif( sys.argv[1].upper() == "LEAGUE"):
-		LeagueName = sys.argv[2]
+	elif( sys.argv[1].upper() == "--HELP"):
+		show_help()
 
-		if( LeagueName is None ):
+	elif( sys.argv[1].upper() == "--LEAGUE"):
+		if (len(sys.argv) < 3):
 			print ("No league name entered !")
+			print ("use --help for the list of command\n")
 		else:
+			LeagueName = sys.argv[2]
 			show_league_table(LeagueName)
-	elif( sys.argv[1].upper() == "NEXTWEEK"):
-		LeagueName = sys.argv[2]
 
-		if( LeagueName is None):
+	elif( sys.argv[1].upper() == "--NEXTWEEK"):
+		if (len(sys.argv) < 3):
 			print("No league entered !")
+			print ("use --help for the list of command\n")
 		else:
+			LeagueName = sys.argv[2]
 			show_next_game(LeagueName)
 
+	elif( sys.argv[1].upper() == "--TEAM"):
+		if (len(sys.argv) < 3):
+			print("No team entered !")
+			print ("use --help for the list of command\n")
+		else:
+			TeamName = sys.argv[2]
+			show_team(TeamName)
 	else :
 		print ("Invalid Argument: "+ sys.argv[1])
+		print ("use --help for the list of command\n")
 
 
 if __name__ == "__main__":
